d233e58ea5687eaa4b83b612d3eb682b8b96c606
patel <parthpatel167@gmail.com>
2023-12-17 14:47:16 -0600

    js file created and ans all question and all are corect

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let facts = { numPlanets: 8, yearNeptuneDiscovered: 1846 };
 let { numPlanets, yearNeptuneDiscovered } = facts;
 
-console.log(numPlanets); // ?
-console.log(yearNeptuneDiscovered); // ?
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
 
 let planetFacts = {
   numPlanets1: 8,
@@ -12,21 +12,21 @@ let planetFacts = {
 
 let { numPlanets1, ...discoveryYears } = planetFacts;
 
-console.log(discoveryYears); // ?
+console.log(discoveryYears); //  {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
 
 function getUserData({ firstName, favoriteColor = 'green' }) {
   return `Your name is ${firstName} and you like ${favoriteColor}`;
 }
 
-getUserData({ firstName: 'Alejandro', favoriteColor: 'purple' }); // ?
-getUserData({ firstName: 'Melissa' }); // ?
-getUserData({}); // ?
+getUserData({ firstName: 'Alejandro', favoriteColor: 'purple' }); //  Your name is Alejandro and you like purpl
+getUserData({ firstName: 'Melissa' }); //  Your name is Melissa and you like green
+getUserData({}); // Your name is undefine and you like green
 
 let [first, second, third] = ['Maya', 'Marisa', 'Chi'];
 
-console.log(first); // ?
-console.log(second); // ?
-console.log(third); // ?
+console.log(first); // Maya
+console.log(second); // Marisa
+console.log(third); // chi
 
 let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
   'Raindrops on roses',
@@ -36,25 +36,33 @@ let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
   'Brown paper packages tied up with strings',
 ];
 
-console.log(raindrops); // ?
-console.log(whiskers); // ?
-console.log(aFewOfMyFavoriteThings); // ?
+console.log(raindrops); // Raindrops on roses
+console.log(whiskers); // whiskers on kittens
+console.log(aFewOfMyFavoriteThings); // ['Bright copper kettles', 'warm woolen mittens', 'Brown paper packages tied up with strings']
 
 let numbers = [10, 20, 30];
 [numbers[1], numbers[2]] = [numbers[2], numbers[1]];
 
-console.log(numbers); // ?
+console.log(numbers); // [10,30,20]
 
-var obj = {
+// var obj = {
+//   numbers: {
+//     a: 1,
+//     b: 2,
+//   },
+// };
+
+// var a = obj.numbers.a;
+// var b = obj.numbers.b;
+//ES2015
+const obj = {
   numbers: {
     a: 1,
     b: 2,
   },
 };
 
-var a = obj.numbers.a;
-var b = obj.numbers.b;
-//ES2015
+const { a, b } = obj.numbers;
 
 var arr = [1, 2];
 var temp = arr[0];
@@ -62,8 +70,7 @@ arr[0] = arr[1];
 arr[1] = temp;
 
 //ES2015
-
-raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre']);
+[arr[0], arr[1]] = [arr[1], arr[0]];
 
 /*
   {
@@ -73,3 +80,10 @@ raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre']);
     rest: ["David", "Pierre"]
   }
 */
+const raceResults = ([first, second, third, ...rest]) => ({
+  first,
+  second,
+  third,
+  rest,
+});
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre']);
